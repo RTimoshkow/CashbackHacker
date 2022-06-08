4fab5910a42ae3b70b7a4c1eb9ff8ba22260623c
Roman Timoshkov <timoshkow@mail.ru>
2022-06-08 15:31:40 +0300

    added junit4

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+
+    CashbackHacker service = new CashbackHacker();
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountNegative() {
+
+        int amount = -1;
+        int actual = service.remain(amount);
+        int expected = 1001;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount2500() {
+
+        int amount = 2500;
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+}
